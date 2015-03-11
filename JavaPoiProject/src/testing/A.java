5f3aa2028390d06d14f5ca285d05391f5396dd47
nitin <testingworldnoida@gmail.com>
2015-03-11 22:40:12 +0530

    Commit

---
+++ JavaPoiProject/src/testing/A.java
@@ -0,0 +1,29 @@
+package testing;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class A {
+
+	public static void main(String aa[]) throws Exception
+	{
+		File f = new File("G:\\Data21.xlsx");
+		FileOutputStream fo = new FileOutputStream(f);
+		XSSFWorkbook wk =  new XSSFWorkbook();
+		XSSFSheet s1 = wk.createSheet();
+		Row r1= s1.createRow(0);
+		Cell c1= r1.createCell(0);
+		c1.setCellValue("HELLO DELHI1");
+		wk.write(fo);
+		fo.flush();
+		fo.close();
+	}
+	
+	
+}
